d69bb9976c185f16ec49b036206075f46dd3f0ea
ss95332 <sanyal.sunny@utexas.edu>
2024-04-13 00:09:11 -0500

    update prepare.sh

---
+++ lit-gpt/prepare.sh
@@ -0,0 +1,15 @@
+# NOTE: specify your own hugging face token to download the models
+export HF_HOME=/scratch/07946/ss95332/huggingface
+
+#download llama2 model
+python scripts/download.py --repo_id meta-llama/Llama-2-7b-hf --access_token writeyourtokenhere
+python scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/meta-llama/Llama-2-7b-hf
+
+# download openllama model
+python scripts/download.py --repo_id openlm-research/open_llama_7b --checkpoint_dir /sample/checkpoints/openlm-research/open_llama_7b
+python scripts/convert_hf_checkpoint.py --checkpoint_dir /sample/checkpoints/openlm-research/open_llama_3b
+
+# download dataset and tokenize
+python scripts/prepare_redpajama.py --source_path data/RedPajama-Data-1T-Sample --checkpoint_dir checkpoints/meta-llama/Llama-2-7b-hf/ --destination_path data/lit-redpajama-sample --sample True
+
+python scripts/prepare_redpajama.py --source_path /sample/data/RedPajama-Data-1T-Sample --checkpoint_dir /sample/checkpoints/meta-llama/Llama-2-7b-hf/ --destination_path data/lit-redpajama-sample --sample True
