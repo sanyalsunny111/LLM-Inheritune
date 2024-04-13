1eb97a9990f46730a84700ffe93b5fa7d2ba6fbb
ss95332 <sanyal.sunny@utexas.edu>
2024-04-12 20:27:37 -0500

    updated train.sh

--- lit-gpt/train.sh
+++ lit-gpt/train.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0
 
-python finetune/full_redpj_epoch.py --precision "bf16-true" --train_data_dir /sample/data/lit-redpajama --checkpoint_dir /sample/checkpoints/openlm-research/open_llama_3b/
+python Training/train.py --precision "bf16-true" --train_data_dir /sample/data/lit-redpajama --checkpoint_dir /sample/checkpoints/openlm-research/open_llama_3b/
 
